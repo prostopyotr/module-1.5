eee52b71074776de74f64a8ef32353504c436529
prostopyotr <petyaKornienko@yandex.com>
2024-11-27 13:23:12 +0400

    Add files via upload

---
+++ module_1_5[strings].py
@@ -0,0 +1,20 @@
+example = 'Палиндром'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:9:2])
+
+example2 = 'Наворован'
+print(example2[0])
+print(example2[-1])
+print(example2[4:])
+print(example2[::-1])
+print(example2[0:10:2])
+
+example3 = 'Интерес'
+print(example3[0])
+print(example3[-3])
+print(example3[2:5])
+print(example3[::-1])
+print(example3[0:7:3])
